eed02afe4458cbb0e33ed06f1c676c4909a69901
Robert Butler <r7butler@yahoo.com>
2023-08-09 17:01:16 -0700

    Temporarily Added the elevation_field function that applies to diferent data types.It will later be moved to where all the global checks are  -Ayah 08/09/2023

--- proj/core/functions.py
+++ proj/core/functions.py
@@ -4,6 +4,7 @@ import re, time
 from math import log10
 from functools import lru_cache
 import datetime
+from flask import g 
 
 def checkData(dataframe, tablename, badrows, badcolumn, error_type, is_core_error = True, error_message = "Error", errors_list = [], q = None):
     if len(badrows) > 0:
@@ -274,5 +275,49 @@ def check_bad_time_format(value):
     """
     correct_time_format =  r'^(0?[0-9]|1\d|2[0-3]):([0-5]\d)(:([0-5]\d))?$'
     return not bool(re.match(correct_time_format, str(value)))
+
+########## Ayah's elevation check function:
+def check_elevation_fields(all_dfs):
+    output = {
+        'core_errors': [],
+        'core_warnings': [],
+    }
+
+    sql = f"""
+        select table_name from information_schema.columns
+        where column_name in ('elevation_ellipsoid','elevation_orthometric')
+        and table_name like 'tbl_%%' 
+        group by table_name 
+    """
+    tbls_with_elevation = pd.read_sql(sql, g.eng)
+
+    #A list of all the columns that need to be checked if empty or not
+    columns_to_check = ['elevation_time','elevation_units','elevation_corr','elevation_datum']
+    #Checking if the table names in all_dfs match the table in database that have the elevation columns 
+    for item in all_dfs.keys():
+        if item in list(tbls_with_elevation['table_name']):
+    #Looping through every column to see if it is empty after filtring out the tables with the elevation columns 
+            for cols in columns_to_check:
+                badrows = all_dfs[item][(all_dfs[item]['elevation_ellipsoid'].notna() | all_dfs[item]['elevation_orthometric'].notna()) & ( all_dfs[item][cols].isna() | (all_dfs[item][cols] == -88) | (all_dfs[item][cols].str.lower() == 'not recorded') )].index.tolist()
+    #Adding them to output dictionary        
+                if len(badrows) > 0:
+                    tmp = {
+                        'table': item,
+                        'rows': badrows,
+                        'columns': cols,
+                        'error_type': 'Elevation Error',
+                        'is_core_error': False,
+                        'error_message': f'{cols} cannot be null or -88 since Elevation_ellipsoid and/or Elevation_orthometric are nonempty '
+                    }
+                    output['core_errors'].append(tmp)
+                        
+    return output
+
+
+                
+
+
+
+
 
 
