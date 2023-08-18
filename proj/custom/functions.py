1573cf8afa1a38b2a9bc68f99337e90f31e8db72
Robert Butler <r7butler@yahoo.com>
2023-08-17 17:01:31 -0700

    Starting to modify the code so it follows the coding standard

--- proj/custom/functions.py
+++ proj/custom/functions.py
@@ -1,5 +1,5 @@
 import json, os
-from pandas import isnull, DataFrame
+from pandas import isnull, DataFrame, read_sql
 
 def checkData(dataframe, tablename, badrows, badcolumn, error_type, is_core_error = False, error_message = "Error", errors_list = [], q = None):
     
@@ -106,3 +106,25 @@ def mismatch(df1, df2, mergecols = None, left_mergecols = None, right_mergecols 
         badrows = []
 
     return badrows
+
+# This function allows you to put in a table name and get back the primary key fields of the table
+def get_primary_key(tablename, eng):
+    # eng is a sqlalchemy database connection
+
+    # This query gets us the primary keys of a table. Not in a python friendly format
+    # Copy paste to Navicat, pgadmin, or do a pd.read_sql to see what it gives
+    pkey_query = f"""
+        SELECT 
+            c.column_name, 
+            c.data_type
+        FROM information_schema.table_constraints tc 
+        JOIN information_schema.constraint_column_usage AS ccu USING (constraint_schema, constraint_name) 
+        JOIN information_schema.columns AS c ON c.table_schema = tc.constraint_schema
+            AND tc.table_name = c.table_name AND ccu.column_name = c.column_name
+        WHERE constraint_type = 'PRIMARY KEY' and tc.table_name = '{tablename}';
+    """
+    pkey_df = read_sql(pkey_query, eng)
+    
+    pkey = pkey_df.column_name.tolist() if not pkey_df.empty else []
+    
+    return pkey
