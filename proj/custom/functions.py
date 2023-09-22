258bd5fb65686ef14e39a83b3ad6a935b6137656
Robert Butler <r7butler@yahoo.com>
2023-09-22 16:35:41 -0700

    added function check_date_order

--- proj/custom/functions.py
+++ proj/custom/functions.py
@@ -263,3 +263,13 @@ def check_consecutiveness(df, groupcols, col_to_check):
 
     badrows = [idx for indexes in df.groupby(groupcols).apply(is_consecutive) for idx in indexes]
     return badrows
+
+def check_date_order(df, before_date, after_date):
+    
+    # This function checks that dates are in the correct order
+
+    df[before_date] = pd.to_datetime(df[before_date], format='%d/%m/%Y').dt.date
+    df[after_date] = pd.to_datetime(df[after_date], format='%d/%m/%Y').dt.date
+    
+    return df[(df[before_date] > df[after_date])].tmp_row.tolist()
+    
