248eac36449521a9787e8ac8dac8a360cf4ca347
Robert Butler <r7butler@yahoo.com>
2023-09-28 18:12:47 -0700

    QAd benthic large

--- proj/custom/benthiclarge_custom.py
+++ proj/custom/benthiclarge_custom.py
@@ -2,7 +2,7 @@
 
 from inspect import currentframe
 from flask import current_app, g
-from .functions import checkData
+from .functions import checkData, get_primary_key, mismatch
 import pandas as pd
 
 def benthiclarge(all_dfs):
@@ -54,5 +54,75 @@ def benthiclarge(all_dfs):
     #   "error_message" : "This is a helpful useful message for the user"
     # })
     # errs = [*errs, checkData(**args)]
+
+    args = {
+        "dataframe": pd.DataFrame({}),
+        "tablename": '',
+        "badrows": [],
+        "badcolumn": "",
+        "error_type": "",
+        "is_core_error": False,
+        "error_message": ""
+    }
+
+    ######################################################################################################################
+    # ------------------------------------------------------------------------------------------------------------------ #
+    # ------------------------------------------------ benthiclarge Logic Checks --------------------------------------- #
+    # ------------------------------------------------------------------------------------------------------------------ #
+    ######################################################################################################################
+
+    benthiclargemeta_pkey = get_primary_key('tbl_benthiclarge_metadata', g.eng)
+    benthiclargeabundance_pkey = get_primary_key('tbl_benthiclarge_abundance', g.eng)
+
+
+    benthiclargemeta_benthiclargeabundance_shared_pkey = [x for x in benthiclargemeta_pkey if x in benthiclargeabundance_pkey]
+
+    print("# CHECK - 1")
+    # Description: Each record in benthiclarge_metadata must include corresponding record in benthiclarge_abundance (🛑 ERROR 🛑)
+    # Created Coder: Duy
+    # Created Date: 9/28/2023
+    # Last Edited Date: 
+    # Last Edited Coder:
+    # NOTE (9/28/2023): Duy created the check, has not QA'ed yet. 
+    args.update({
+        "dataframe": benthiclargemeta,
+        "tablename": "tbl_benthiclarge_metadata",
+        "badrows": mismatch(benthiclargemeta, benthiclargeabundance, benthiclargemeta_benthiclargeabundance_shared_pkey), 
+        "badcolumn": ','.join(benthiclargemeta_benthiclargeabundance_shared_pkey),
+        "error_type": "Logic Error",
+        "error_message": "Each record in benthiclarge_metadata must include corresponding record in benthiclarge_abundance"
+    })
+    errs = [*errs, checkData(**args)]
+
+    print("# END OF CHECK - 1")
+
+    print("# CHECK - 2")
+    # Description: Each record in benthiclarge_abundance must include corresponding record in benthiclarge_metadata (🛑 ERROR 🛑)
+    # Created Coder: Duy
+    # Created Date: 9/28/2023
+    # Last Edited Date: 
+    # Last Edited Coder:
+    # NOTE (9/28/2023): Duy created the check, has not QA'ed yet. 
+    args.update({
+        "dataframe": benthiclargeabundance,
+        "tablename": "tbl_benthiclarge_abundance",
+        "badrows": mismatch(benthiclargeabundance, benthiclargemeta, benthiclargemeta_benthiclargeabundance_shared_pkey), 
+        "badcolumn": ','.join(benthiclargemeta_benthiclargeabundance_shared_pkey),
+        "error_type": "Logic Error",
+        "error_message": "Each record in benthiclarge_abundance must include corresponding record in benthiclarge_metadata"
+    })
+    errs = [*errs, checkData(**args)]
+
+    print("# END OF CHECK - 2")
+    
+    
+    
+    ######################################################################################################################
+    # ------------------------------------------------------------------------------------------------------------------ #
+    # ------------------------------------------------end of benthiclarge Logic Checks --------------------------------- #
+    # ------------------------------------------------------------------------------------------------------------------ #
+    ######################################################################################################################
+
+
 
     return {'errors': errs, 'warnings': warnings}
