f240610ffbf78b2e62d522badf5e287fbf4206fa
Robert Butler <r7butler@yahoo.com>
2024-01-05 17:42:21 -0800

    field grab check 3, updated description to include QA comment field grab check 3 updated to check the following: if matrix is sediment and collectionmethod is sediment core, then coresizedepth must be filled (CANNOT be -88), updated, tested and QA by zaib

--- proj/custom/grab_field_custom.py
+++ proj/custom/grab_field_custom.py
@@ -199,7 +199,7 @@ def grab_field(all_dfs):
     # NOTE (09/12/2023): Ayah adjusted the format so it follows the coding standard
     # NOTE (09/25/2023): Aria adjusted error message and changed code since coresizediamerter is numeric so -88 is the numeric equivalent to "Not Recorded"
     # NOTE (09/26/2023): Aria changed the logic of this check (changed from Not recorded to -88 since coresizediameter is a numeric column).
-    # NOTE (01/05/2024): Zaib updated description with specific criteria provided by Jan. 
+    # NOTE (01/05/2024): Zaib updated description with specific criteria provided by Jan. QA done.
 
     args.update({
         "dataframe":grabeventdet,
@@ -218,26 +218,28 @@ def grab_field(all_dfs):
     print("# END OF CHECK - 3")
 
     print("# CHECK - 4")
-    # Description: coresizedepth should be filled when matrix is sediment 
+    # Description: If matrix is "sediment" and collectionmethod is "sediment core", then coresizedepth MUST be a numeric value (CANNOT be -88)
     # Created Coder: Ayah 
     # Created Date: NA
-    # Last Edited Date: 09/26/2023
-    # Last Edited Coder: Aria Askaryar
+    # Last Edited Date: 01/05/2024
+    # Last Edited Coder: Zaib Quraishi
     # NOTE (09/12/2023): Ayah adjusted the format so it follows the coding standard
     # NOTE (09/25/2023): Aria adjusted error message and changed code since coresizedepth is numeric so -88 is the numeric equivalent to "Not Recorded"
     # NOTE (09/26/2023): Aria changed the logic of this check (changed from Not recorded to -88 since coresizediameter is a numeric column).
+    # NOTE (01/05/2024): Zaib updated description and check based on specified criteria provided by Jan. QA done.
 
     args.update({
             "dataframe":grabeventdet,
             "tablename":'tbl_grabevent_details',
             "badrows":grabeventdet[
                 (grabeventdet['matrix'] == 'sediment') &
-                (grabeventdet['coresizedepth'] != -88) 
+                (grabeventdet['collectionmethod'] == 'sediment core') &
+                (grabeventdet['coresizedepth'] == -88) 
             ].tmp_row.tolist(),
             "badcolumn": "coresizedepth",
             "error_type": "empty value",
             "is_core_error": False,
-            "error_message": "When matrix is sediment, then you should put -88 for coresizedepth column"
+            "error_message": "If matrix is sediment and collectionmethod is sediment core, then coresizedepth MUST be a numeric value (CANNOT be -88)."
     })
     errs = [*errs, checkData(**args)]
 
