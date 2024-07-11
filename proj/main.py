e1968f16abe0a28cee241b2bc44ef8c021086d9d
Robert Butler <r7butler@yahoo.com>
2024-07-11 11:12:13 -0700

    added feldspar_data to the allowable sheets that can be blank. user can leave this sheet blank if plug_extracted = 'no'. if plug_extract='yes', then there has to be data and there is a check for it.

--- proj/main.py
+++ proj/main.py
@@ -152,7 +152,7 @@ def main():
             ]
         )
         # make sure all tabs are filled with data with the only exception for cordgrass table
-        if (tblname != 'tbl_cordgrass') and (all_dfs[tblname].empty):
+        if (tblname not in ['tbl_cordgrass', 'tbl_feldspar_data']) and (all_dfs[tblname].empty):
             return jsonify(user_error_msg=f'Please fill out the tab {tblname} before you continue')
     print("DONE - building 'all_dfs' dictionary")
 
