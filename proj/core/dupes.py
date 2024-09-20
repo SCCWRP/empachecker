a51b88b33f9459bdc07a8487b0c684dad3199052
Robert Butler or someone else at SCCWRP <robertb@sccwrp.org>
2024-09-20 21:16:25 +0000

    changed the way core checkdupinproduction works

--- proj/core/dupes.py
+++ proj/core/dupes.py
@@ -53,9 +53,6 @@ def checkDuplicatesInSession(dataframe, tablename, eng, *args, output = None, **
 
 
 def checkDuplicatesInProduction(dataframe, tablename, eng, *args, output = None, **kwargs):
-    """
-    check for duplicates in Production only
-    """
     print("BEGIN function - checkDuplicatesInProduction")
     
     if session.get("final_submit_requested") == False:
@@ -64,77 +61,61 @@ def checkDuplicatesInProduction(dataframe, tablename, eng, *args, output = None,
     pkey = get_primary_key(tablename, eng)
     print(pkey)
     
+
     # initialize return values
     ret = []
 
     if len(pkey) == 0:
         print("No Primary Key")
         return ret
-
-   
-    current_recs = read_sql(f"SELECT DISTINCT {','.join(pkey)} FROM {tablename}", eng)
     
-    import time
-    print("are there current_recs in the database?")
-    print(not current_recs.empty)
-    time.sleep(1)
-    if not current_recs.empty:
+    dtype = session.get('datatype')
+    submissionid = session.get('submissionid')
+    
+    if not dtype or not submissionid:
+        raise ValueError("Session does not contain 'dtype' or 'submissionid'")
+    
+    # Set the table name
+    tmp_table_name = f'tmp_{dtype}_{submissionid}'
+    
+    # Load the dataframe to the database table
+    dataframe.to_sql(tmp_table_name, con=eng, if_exists='replace', index=False)
+
+    # SQL query to get common rows based on primary key
+    join_condition = " AND ".join([f"tmp.{col} = tbl.{col}" for col in pkey])
+
+    # SQL query to get common rows based on the primary keys
+    query = f"""
+        SELECT tmp.* 
+        FROM {tmp_table_name} tmp
+        INNER JOIN {tablename} tbl
+        ON {join_condition};
+    """
+    
+    print("Executing duplicate check query...")
+    
+    # Execute the query and return the result as a DataFrame
+    duplicates_df = pd.read_sql_query(query, eng)
+    badrows = duplicates_df.index.tolist()
+
 
     
-        for dt in list(zip(current_recs.dtypes.index, current_recs.dtypes)):
-            col = dt[0]
-            typ = dt[1]
-            assert len(dt) > 1, "Error in dupes - some item in current_recs.dtypes didnt have a length greater than 1"
-            assert col in dataframe.columns, f"supposed primary key column {col} not found in columns of the dataframe that was matched with {tablename}"
+    ret = [
+        checkData(
+            dataframe = dataframe,
+            tablename = tablename,
+            badrows = badrows,
+            badcolumn = ','.join([col for col in pkey if col not in current_app.system_fields]),
+            error_type = "Duplicate",
+            is_core_error = True,
+            error_message = "This is a record which already exists in the database"
+        )
+    ]
 
-            try:
-                # Coerce datatypes of primary key columns to match so that the two dataframes can merge 
-                if typ == 'object':
-                    dataframe[col] = dataframe[col].astype(typ).apply(lambda x: str(x).strip())
-                    current_recs[col] = current_recs[col].astype(typ).apply(lambda x: str(x).strip())
-                else:
-                    dataframe[col] = dataframe[col].astype(typ)
-            except Exception as e:
-                # An exception should only occur if the column was not able to be coerced to the correct datatype, in which case the datatypes check should have caught it
-                print(e)
-                if output:
-                    output.put(ret)
-                return ret
-                
+    if output:
+        output.put(ret)
 
-        # tack on a column to identify records that have been prviously submitted
-        current_recs = current_recs.assign(already_in_db = True)
+    eng.execute(f"DROP TABLE IF EXISTS {tmp_table_name};")
 
-        # merge current recs with a left merge and tack on that "already_in_db" column
-        dataframe = dataframe.merge(current_recs, on = pkey, how = 'left')
-
-        badrows = dataframe[dataframe.already_in_db == True].index.tolist()
-
-        print("badrows")
-        print(badrows)
-        
-        print("tablename")
-        print(tablename)
-        
-        time.sleep(1)
-
-        ret = [
-            checkData(
-                dataframe = dataframe,
-                tablename = tablename,
-                badrows = badrows,
-                badcolumn = ','.join([col for col in pkey if col not in current_app.system_fields]),
-                error_type = "Duplicate",
-                is_core_error = True,
-                error_message = "This is a record which already exists in the database"
-            )
-        ]
-
-        if output:
-            output.put(ret)
-
-        
     print("END function - checkDuplicatesInProduction")
     return ret
-
-
